edf590de75b6f3ca499ece99387bd98635324df8
vuongnd9 <nguyendinhvuong.act@gmail.com>
2026-03-28 22:29:52 +0700

    draft content

--- Program.cs
+++ Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var targetNumber = Random.Shared.Next(1, 101);
+        var maxAttempt = 10;
+        var attempt = 0;
+        while (true)
+        {
+            Console.Write("> Enter your guess: ");
+            var guessNumberStr = Console.ReadLine();
+            if (!int.TryParse(guessNumberStr, out var guessNumber))
+            {
+                System.Console.WriteLine("Your input should be numeric");
+            }
+            if (guessNumber < 1 || guessNumber > 100)
+            {
+                System.Console.WriteLine("Your guess is out of range");
+            }
+
+            attempt++;
+
+            if (guessNumber > targetNumber)
+            {
+                System.Console.WriteLine($"Incorrect! The number is less than {guessNumber}.");
+            }
+            else if (guessNumber < targetNumber)
+            {
+                System.Console.WriteLine($"Incorrect! The number is greater than {guessNumber}.");
+            }
+            else
+            {
+                System.Console.WriteLine($"Congratulations! You guessed the correct number in {attempt} attempts");
+                return;
+            }
+
+            if (attempt == maxAttempt)
+            {
+                System.Console.WriteLine("You're FAIL");
+            }
+
+        }
     }
 }
